776cf3ce254cd8c63677be051aff21e327ae27b6
haiyangsui <haiyangsui@gmail.com>
2009-10-30 16:22:32 +0000

    Updated FAP.aspx, FAPMaintenance.aspx by Haiyang on 31-Oct-09.

--- CMP/SOURCE/CMPProject/branches/Haiyang/20091030/Web/ACS/FAPMaintenance.aspx.cs
+++ CMP/SOURCE/CMPProject/branches/Haiyang/20091030/Web/ACS/FAPMaintenance.aspx.cs
@@ -10,11 +10,29 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
+using WorkLayers.BusinessLayer;
 
 public partial class FAPMaintenance : System.Web.UI.Page
 {
+    FAPBL fapBL;
+
+    public FAPMaintenance()
+    {
+        fapBL = new FAPBL();
+        //PopulateFAPList();
+    }
+
     protected void Page_Load(object sender, EventArgs e)
     {
         ConfigurationManager.AppSettings["CurrentMenu"] = "PortalAdmin";
+        PopulateFunctions();
     }
+
+    private void PopulateFunctions()
+    {
+        DataTable dtFunctions = fapBL.GetAllFAPs();
+        gv_Functions.DataSource = dtFunctions;
+        gv_Functions.DataBind();
+    }
+
 }
