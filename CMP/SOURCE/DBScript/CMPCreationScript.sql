2eb653cdfdeb3481e5bb0a60936efecbe2e1180c
haiyangsui <haiyangsui@gmail.com>
2009-11-06 15:28:24 +0000

    Modified Structure of Module_Master.

--- CMP/SOURCE/DBScript/CMPCreationScript.sql
+++ CMP/SOURCE/DBScript/CMPCreationScript.sql
@@ -1309,3 +1309,41 @@ INSERT INTO [FAP_Master]([FAPListID],[FAPFunctionID]) VALUES (3,4)
 
 /********************************* End of Addition ********************************************/
 
+
+/******************************* Added by Haiyang on 06-Nov-09 *******************************/
+-- here to change the table structure for Module_Master
+
+-- drop original Module_Master first 
+
+/****** Object:  Table [dbo].[module_master]    Script Date: 11/06/2009 23:25:53 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[module_master]') AND type in (N'U'))
+DROP TABLE [dbo].[module_master]
+
+
+-- Add in new table Module_Master
+/****** Object:  Table [dbo].[Module_Master]    Script Date: 11/06/2009 23:27:26 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Module_Master](
+	[module_id] [int] IDENTITY(1,1) NOT NULL,
+	[module_name] [varchar](50) NOT NULL,
+	[module_remarks] [varchar](500) NOT NULL,
+	[created_by] [varchar](50) NOT NULL,
+	[created_date] [datetime] NOT NULL,
+	[modified_by] [varchar](50) NOT NULL,
+	[modified_date] [datetime] NOT NULL,
+	[delete_flag] [varchar](2) NULL,
+ CONSTRAINT [pk_module_master] PRIMARY KEY CLUSTERED 
+(
+	[module_id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+
+/****************************** End of Addition on 06-Nov-09 *********************************/
